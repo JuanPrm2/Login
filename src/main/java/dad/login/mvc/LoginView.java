f754b35f62796a2606c6f4705f9861b7160c2b00
JuanPrm2 <juan28720027@gmail.com>
2021-10-21 13:36:36 +0100

    Update LoginView.java

--- src/main/java/dad/login/mvc/LoginView.java
+++ src/main/java/dad/login/mvc/LoginView.java
@@ -1,10 +1,12 @@
 package dad.login.mvc;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class LoginView {
 
@@ -28,5 +30,52 @@ public class LoginView {
 		acceder=new Button();
 		cancelar= new Button();
 		
+		
+		HBox primeraFila = new HBox(usuarioLabel,usuarioTextField);
+		primeraFila.setAlignment(Pos.CENTER);
+		
+		HBox segundaFila= new HBox (contraseniaLabel,contraseniaPasswordField);
+		segundaFila.setAlignment(Pos.CENTER);
+		
+		HBox terceraFila= new HBox(usarCheckBox,usarLabel);
+		terceraFila.setAlignment(Pos.CENTER);
+		
+		HBox cuartafila= new HBox(acceder,cancelar);
+		cuartafila.setAlignment(Pos.CENTER);
+		
+		
+		
+	}
+
+	public TextField getUsuarioTextField() {
+		return usuarioTextField;
+	}
+
+	public Label getUsuarioLabel() {
+		return usuarioLabel;
+	}
+
+	public Label getContraseniaLabel() {
+		return contraseniaLabel;
+	}
+
+	public Label getUsarLabel() {
+		return usarLabel;
+	}
+
+	public CheckBox getUsarCheckBox() {
+		return usarCheckBox;
+	}
+
+	public PasswordField getContraseniaPasswordField() {
+		return contraseniaPasswordField;
+	}
+
+	public Button getAcceder() {
+		return acceder;
+	}
+
+	public Button getCancelar() {
+		return cancelar;
 	}
 }
